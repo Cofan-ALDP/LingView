f3c7785295fffa538694ce810b065fdbcc4e2f41
Kalinda Pride (XPS) <kalinda.pride@gmail.com>
2021-01-04 21:30:40 -0800

    fix link to pdf

--- jsx/App/Materials/MaterialCard.jsx
+++ jsx/App/Materials/MaterialCard.jsx
@@ -13,14 +13,14 @@ export function MaterialCard({ metadata }) {
 	return (
         <div className="material-card">
             <aside>
-                <a href={metadata.savedPath}>
+                <a href={metadata.itemServerUrl}>
                     { metadata.thumbnailImageUrl
                         ? <img src={metadata.thumbnailImageUrl} />
                         : <div className="nothumbnail"><span>no preview</span></div> }
                 </a>
             </aside>
             <main>
-                <a href={metadata.savedPath}><h3>{metadata.title}</h3></a>
+                <a href={metadata.itemServerUrl}><h3>{metadata.title}</h3></a>
                 <h4>{metadata.credits.concat([metadata.year]).filter(x => x).join(', ')}</h4>
                 <p dangerouslySetInnerHTML={{__html: metadata.descriptionHTML}} /> 
                 {/* it's okay since we sanitize ahead of time (see fetch-materials.js) */}
